9a56d7232bd6f12b851cb97a269bc5b403d21003
raushanmahmud <raushanm.re@gmail.com>
2020-09-17 17:02:10 +0500

    change controller

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Raushan";
 
             return View();
         }
